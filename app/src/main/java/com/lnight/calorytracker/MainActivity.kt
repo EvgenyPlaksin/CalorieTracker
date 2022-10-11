168683c6bcb7c7f6271c574428f45a7e1f8a9a14
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2022-10-11 13:11:52 +0300

    gender screen

--- app/src/main/java/com/lnight/calorytracker/MainActivity.kt
+++ app/src/main/java/com/lnight/calorytracker/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import com.lnight.calorytracker.navigation.navigate
 import com.lnight.calorytracker.ui.theme.CaloryTrackerTheme
 import com.lnight.core.navigation.Route
+import com.lnight.onboarding_presentation.gender.GenderScreen
 import com.lnight.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
